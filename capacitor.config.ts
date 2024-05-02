993b5c80291ba49a36e1ba766eb1c51336499de5
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-02 16:09:21 -0300

    Base

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.pp4.app',
   appName: 'PPS-PP-App4',
   webDir: 'www'
 };
