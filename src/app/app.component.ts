283ee2896a5e2094865c78c2ddefce970b33637c
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-02 23:40:39 -0300

    Splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private navCtrl: NavController) {}
+
+  ngOnInit() {
+    this.navCtrl.navigateRoot('splash');
+  }
 }
---
+++ src/app/splash/splash.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+})
+export class SplashPage {
+  constructor(private navCtrl: NavController) {
+    setTimeout(() => {
+      navCtrl.navigateRoot('/home');
+      history.pushState(null, '');
+    }, 2650);
+  }
+}
