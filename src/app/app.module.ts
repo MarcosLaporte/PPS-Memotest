be89d369299a52b596054d1962ef4c3be90d9ebe
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-10 14:29:19 -0300

    Lots of styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -13,15 +13,22 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({
-    "projectId":"primer-parcial-app1",
-    "appId":"1:915380151213:web:e25ef4b9543b87809e7724",
-    "storageBucket":"primer-parcial-app1.appspot.com",
-    "apiKey":"AIzaSyBiLy7FGCZADs_CuYXVrsnUUvmT8sWfXtc",
-    "authDomain":"primer-parcial-app1.firebaseapp.com",
-    "messagingSenderId":"915380151213"
-  })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "primer-parcial-app1",
+      "appId": "1:915380151213:web:e25ef4b9543b87809e7724",
+      "storageBucket": "primer-parcial-app1.appspot.com",
+      "apiKey": "AIzaSyBiLy7FGCZADs_CuYXVrsnUUvmT8sWfXtc",
+      "authDomain": "primer-parcial-app1.firebaseapp.com",
+      "messagingSenderId": "915380151213"
+    })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule {}
+export class AppModule { }
