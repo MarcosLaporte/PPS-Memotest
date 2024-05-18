0b888386992046f237b397ad7bd64abcb3ef6064
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-18 16:32:53 -0300

    Splsh screen plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.pp4.app',
   appName: 'PPS-PP-App4',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchAutoHide: true,
+      launchFadeOutDuration: 500,
+      launchShowDuration: 1000,
+      splashFullScreen: false
+    }
+  }
 };
 
 export default config;
